b423d53ee2b9d3cb1f224d4f0e054098de1e494a
jccho <jccho@c-maker.co.kr>
2017-09-26 23:47:34 +0900

    question

--- controller/question.js
+++ controller/question.js
@@ -6,5 +6,11 @@ router.get('/group', function(req, res, next) {
     res.render('question/group', { title: 'Express' });
 });
 
+router.get('/group/detail/:code', function(req, res) {
+    var group_code = req.params.code;
+
+    res.render('question/group/detail', { group_code : group_code });
+});
+
 
 module.exports = router;
