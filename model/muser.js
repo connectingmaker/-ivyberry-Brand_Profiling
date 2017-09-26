af2e4036500f1e66d246e7ed1ab2d5182b77e9c4
kwangheejung <khjung@c-maker.co.kr>
2017-09-26 17:50:33 +0900

    회원리스트 및 추가 80% 완료

--- model/muser.js
+++ model/muser.js
@@ -19,6 +19,68 @@ var muser = {
         connection.end();
         return data;
     }
+    ,getMemberSelect: function(uid, callback) {
+        var connection = mysql_dbc.init();
+        var query = " SELECT * FROM MEMBER WHERE UID = ?";
+        var params = [];
+        params.push(uid);
+
+        var data = connection.query(query,params,callback);
+        connection.end();
+        return data;
+    }
+    ,getEmailCount: function(useremail, callback) {
+        var connection = mysql_dbc.init();
+        var query = " SELECT COUNT(*) AS TOTAL FROM MEMBER WHERE USEREMAIL = ? ";
+        var params = [];
+        params.push(useremail);
+
+        var data = connection.query(query,params,callback);
+        connection.end();
+        return data;
+    }
+    ,getMemberList: function(page, callback) {
+        var connection = mysql_dbc.init();
+        var query = " SELECT ";
+        query += " M.UID ";
+        query += " , M.USERNAME ";
+        query += " , M.USEREMAIL ";
+        query += " , M.SEX ";
+        query += " , M.BIRTHDAY ";
+        query += " , M.POINT ";
+        query += " , M.JOIN_SURVEY_CNT ";
+        query += " , FROM_UNIXTIME(M.LAST_LOGIN) AS LAST_LOGIN ";
+        query += " , FROM_UNIXTIME(M.INSERT_DATETIME) AS INSERT_DATETIME ";
+        query += " , CG.CODE_GRADE ";
+        query += " , CG.CODE_NAME ";
+        query += " , CG.CODE_DESC ";
+        query += " FROM MEMBER M INNER JOIN CODE_GRADE CG ON(M.CODE_GRADE = CG.CODE_GRADE) ";
+        query += " ORDER BY M.INSERT_DATETIME DESC ";
+        query += " LIMIT "+page+", 10";
+
+
+
+        var params = [];
+
+        var data = connection.query(query,params,callback);
+        connection.end();
+        return data;
+    }
+    ,sp_MEMBER_SAVE: function(uid, code_grade, username, useremail, sex, birthday, callback) {
+        var connection = mysql_dbc.init();
+        var query = " call sp_MEMBER_SAVE(?, ?, ?, ?, ?, ?)";
+        var params = [];
+        params.push(uid);
+        params.push(code_grade);
+        params.push(username);
+        params.push(useremail);
+        params.push(sex);
+        params.push(birthday);
+
+        var data = connection.query(query,params,callback);
+        connection.end();
+        return data;
+    }
 
 
 }
