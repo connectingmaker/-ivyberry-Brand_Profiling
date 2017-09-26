490bb05f119005637b48d2ff32243791f373bffa
jccho <jccho@c-maker.co.kr>
2017-09-26 18:11:45 +0900

    statistics

---
+++ public/view_js/statistics/group.js
@@ -0,0 +1,9 @@
+$(function() {
+  var menu = "";
+   $(".top_gnb_menu_sub").show();
+   menu = "<ul><li>완료 설문</li>";
+   menu += "<li>카테고리별 통계</li>";
+    menu += "<li>질문그룹별 통계</li></ul>";
+
+   $(".top_gnb_menu_sub .menu .menu_btn").append(menu);
+});
