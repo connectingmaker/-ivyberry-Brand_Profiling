81fb0695107c51da6ac15df9aca7aa5a5fd4ee04
jccho <jccho@c-maker.co.kr>
2017-09-28 22:55:54 +0900

    20170928

--- public/view_js/brand/list.js
+++ public/view_js/brand/list.js
@@ -64,14 +64,16 @@ $(function() {
 
 
     $(document).on('click', '.deleteBtn', function() {
-        var id = $(this).parents("tr").attr("id");
-        var params = {
-            category_code : id
-        };
+        if(confirm("해당 브랜드 카테고리를 정말로 삭제하시겠습니까?") == true) {
+            var id = $(this).parents("tr").attr("id");
+            var params = {
+                category_code: id
+            };
 
-        common.ajax.send("/brand/categoryDelete", params);
-        common.ajax.return = function(data) {
-            $("#"+data.category_code).remove();
+            common.ajax.send("/brand/categoryDelete", params);
+            common.ajax.return = function (data) {
+                $("#" + data.category_code).remove();
+            }
         }
 
     });
