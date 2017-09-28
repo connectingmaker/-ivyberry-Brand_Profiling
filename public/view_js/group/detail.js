484f869bae7ca69686c1c71ba21f0e483be17725
jccho <jccho@c-maker.co.kr>
2017-09-28 10:59:48 +0900

    20170928

--- public/view_js/group/detail.js
+++ public/view_js/group/detail.js
@@ -1,12 +1,27 @@
 
 $(function() {
+    <!--이미지 7점 척도형 질문 추가하기 -->
+    $("#img_7scaleCreate").click(function() {
+        $('#img_7scaleModal').modal('show');
+    });
+
     <!--7점 척도형 질문 추가하기 -->
     $("#7scaleCreate").click(function() {
         $('#7scaleModal').modal('show');
     });
+
     <!--이미지 선택 / 주관식 질문 추가하기 -->
     $("#imgTextCreate").click(function() {
         $('#imgTextModal').modal('show');
+    });
+
+    <!--양자택일형 질문 추가하기 -->
+    $("#radioCreate").click(function() {
+        $('#radioModal').modal('show');
+    });
+    <!--선택형 질문 추가하기 -->
+    $("#multiCreate").click(function() {
+        $('#multiModal').modal('show');
     });
 
 
