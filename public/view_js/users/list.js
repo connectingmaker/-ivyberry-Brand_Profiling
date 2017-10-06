91ab6a97036622e0e1c0301e4c127267a666611a
jccho <jccho@c-maker.co.kr>
2017-10-06 15:59:22 +0900

    포인트히스토리 팝업

--- public/view_js/users/list.js
+++ public/view_js/users/list.js
@@ -14,20 +14,6 @@ $(function() {
         }
 
     });
-
-    $(document).on('click', '.historyBtn', function() {
-
-            var uid = $(this).parents("tr").attr("uid");
-            var params = {
-                uid: uid
-            };
-
-            common.ajax.send("/users/pointHistory", params);
-            common.ajax.return = function (data) {
-                $('#historyModal').modal('show');
-            }
-
-
-    });
+    
 
 });
