8982eed59a227783266c601beb2bd5a87ec2aa42
jccho <jccho@c-maker.co.kr>
2017-12-05 18:16:44 +0900

    admin notice

---
+++ model/mnotice.js
@@ -0,0 +1,47 @@
+/**
+ * Created by jccho on 2017. 12. 5..
+ */
+var mysql_dbc = require('../module/db_con')();
+
+
+
+
+var mnotice = {
+    get_notice_list: function(callback) {
+    var connection = mysql_dbc.init();
+
+        var query = " SELECT SEQ,SUBJECT,CONTENTS,USE_YN,INSERT_DATETIME,CASE USE_YN WHEN 'N' THEN '미노출' WHEN 'Y' THEN '노출' END USE_YN_TEXT FROM NOTICE ORDER BY INSERT_DATETIME";
+        var params = [];
+        params.push();
+
+        var data = connection.query(query, params, callback);
+        connection.end();
+        return data;
+
+    }
+    ,delnotice: function(seq, callback) {
+        var connection = mysql_dbc.init();
+        var query = " DELETE FROM NOTICE WHERE seq = ? ";
+
+        var params = [];
+        params.push(seq);
+
+        var data = connection.query(query,params,callback);
+        connection.end();
+        return data;
+    }
+    ,get_notice_select: function(seq, callback) {
+        var connection = mysql_dbc.init();
+        var query = " SELECT SEQ,SUBJECT,CONTENTS,USE_YN,INSERT_DATETIME,CASE USE_YN WHEN 'N' THEN '미노출' WHEN 'Y' THEN '노출' END USE_YN_TEXT FROM NOTICE WHERE seq = ? ";
+        var params = [];
+        params.push(seq);
+
+        var data = connection.query(query,params,callback);
+        connection.end();
+        return data;
+    }
+
+
+}
+
+module.exports = mnotice;
---
+++ public/view_js/notice/list.js
@@ -0,0 +1,33 @@
+$(function() {
+    $(document).on('click', '.deleteBtn', function() {
+        if(confirm("해당 공지사항을 정말로 삭제하시겠습니까?") == true) {
+            var id = $(this).parents("tr").attr("id");
+            var params = {
+                seq: id
+            };
+
+
+            common.ajax.send("/notice/noticeDelete", params);
+
+            common.ajax.return = function(data) {
+                var dataJson = data;
+
+                switch(dataJson.ERR_CODE) {
+                    case "000":
+                        $("#"+dataJson.seq).remove();
+                        break;
+                    default:
+                        alert("ERR : "+dataJson.ERR_CODE+"\n\n"+dataJson.ERR_MSG);
+                        break;
+                }
+
+            };
+
+        }
+    });
+
+
+
+});/**
+ * Created by jccho on 2017. 12. 5..
+ */
