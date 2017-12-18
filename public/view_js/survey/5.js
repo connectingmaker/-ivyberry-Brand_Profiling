a3efd03e633bbe11ef6652311ea72ed4efe98e28
kwangheejung <khjung@c-maker.co.kr>
2017-12-18 14:35:55 +0900

    평가 5타입 키워드

--- public/view_js/survey/5.js
+++ public/view_js/survey/5.js
@@ -58,6 +58,9 @@ $(function() {
             ,qaData : JSON.stringify(qaData)
         };
 
+        console.log(json);
+        return;
+
 
 
         common.ajax.send('/survey/multiProcess', json);
@@ -72,47 +75,6 @@ $(function() {
 
 
         }
-
-
-        
-
-        /*
-        if($(".chk_qa_code").is(":checked") == false) {
-            alert("브랜드를 선택해주세요.");
-        } else {
-            var qaData = [];
-            $(".chk_qa_code").each(function() {
-                if($(this).is(":checked") == true) {
-                    var data = {
-                        "qaCode" : $(this).val()
-                    }
-
-                    qaData.push(data);
-                }
-            });
-            var json = {
-                campaign_code : $("#campaign_code").val()
-                ,uid : $("#uid").val()
-                ,seq : $("#seq").val()
-                ,quest_num : $("#quest_num").val()
-                ,page : $("#page").val()
-                ,q_code : $("#q_code").val()
-                ,qaData : JSON.stringify(qaData)
-            };
-
-
-            common.ajax.send('/survey/multiProcess', json);
-            common.ajax.return = function(data) {
-                if(data.PAGE == 0) {
-                    location.replace("/survey/surveyEnd?campaign_code="+$("#campaign_code").val()+"&uid="+$("#uid").val()+"&seq="+$("#seq").val()+"&quest_num="+$("#quest_num").val());
-                } else {
-                    location.replace("/survey/page?campaign_code="+$("#campaign_code").val()+"&uid="+$("#uid").val()+"&seq="+$("#seq").val()+"&quest_num="+$("#quest_num").val()+"&page="+data.PAGE);
-                }
-                //
-            }
-
-        }
-        */
 
     });
 });
