b6c124d5fdd0c18a17491334ce45c27587c5bfab
jccho <jccho@c-maker.co.kr>
2017-12-18 16:55:14 +0900

    5 js

--- public/view_js/survey/5.js
+++ public/view_js/survey/5.js
@@ -1,7 +1,106 @@
 $(function() {
+    $(".qaList").click(function() {
+        var brandNum = $(this).attr("brandNum");
+        var qaCode = $(this).attr("qaCode");
+
+
+
+        if($("#qaData_"+brandNum+qaCode).is(":checked") == false) {
+            $(this).removeClass("btn-white");
+            $(this).addClass("btn-select");
+            var src = $(this).find(".pull-left").find("img").attr("src");
+            src = src.replace("_off", "_on");
+            $(this).find(".pull-left").find("img").attr("src", src);
+            $("#qaData_"+brandNum+qaCode).prop("checked", "checked");
+
+        } else {
+            $(this).removeClass("btn-select");
+            $(this).addClass("btn-white");
+            var src = $(this).find(".pull-left").find("img").attr("src");
+            src = src.replace("_on", "_off");
+            $(this).find(".pull-left").find("img").attr("src", src);
+            $("#qaData_"+brandNum+qaCode).prop("checked", false);
+        }
+
+        var checkedCnt = 0;
+        $(".qaData_"+brandNum).each(function() {
+
+            if($(this).is(":checked") == true) {
+                checkedCnt++;
+            }
+        });
+
+        $("#textselectCnt_"+brandNum).html(checkedCnt+"개가 선택되었습니다.");
+
+
+
+    });
+
+    $(".nextStep").click(function() {
+        var brandSelect = $("#brandSelect").val();
+        var brandCnt = $("#brandCnt").val();
+
+        if(parseInt(brandCnt) > parseInt(brandSelect)) {
+            var tempbrandSelect = brandSelect;
+            brandSelect = parseInt(brandSelect) + 1;
+
+            $("#brandSelect").val(brandSelect);
+            $("#brand"+tempbrandSelect).hide();
+            $("#brand"+brandSelect).show();
+        } else {
+
+            var debugUrl = "";
+
+            if($("#debug").val() == "true") {
+                debugUrl = "&debug=true";
+            }
+            var checkBool = true;
+            var brandCnt = $("#brandCnt").val();
+            var qaData = [];
+            for(var i = 0; i<brandCnt; i++){
+                $(".qaData_"+i).each(function() {
+                    $(this).each(function() {
+                        if($(this).is(":checked") == true) {
+                            var data = {
+                                "brandCode": $(this).attr("brand_code")
+                                , "qaCode": $(this).val()
+                            }
+                            qaData.push(data);
+                        }
+                    });
+
+                });
+            }
+
+            var json = {
+                campaign_code : $("#campaign_code").val()
+                ,uid : $("#uid").val()
+                ,seq : $("#seq").val()
+                ,quest_num : $("#quest_num").val()
+                ,page : $("#page").val()
+                ,q_code : $("#q_code").val()
+                ,qaData : JSON.stringify(qaData)
+            };
+
+            common.ajax.send('/survey/multiProcess', json);
+            common.ajax.return = function(data) {
+
+
+                if(data.PAGE == 0) {
+                    location.replace("/survey/surveyEnd?campaign_code="+$("#campaign_code").val()+"&uid="+$("#uid").val()+"&seq="+$("#seq").val()+"&quest_num="+$("#quest_num").val()+debugUrl);
+                } else {
+                    location.replace("/survey/page?campaign_code="+$("#campaign_code").val()+"&uid="+$("#uid").val()+"&seq="+$("#seq").val()+"&quest_num="+$("#quest_num").val()+"&page="+data.PAGE+debugUrl);
+                }
+
+
+            }
+        }
+    });
+
+    /*
     var brandCnt = $("#brandCnt").val();
     $(".qaData").click(function() {
-        /*
+
         var checkedCnt = 0;
         for(var i = 0; i<brandCnt; i++) {
             if($(".qaData_"+i).is(":checked") == true) {
@@ -12,7 +111,7 @@ $(function() {
         $("#selectCnt").html(checkedCnt);
         $("#textselectCnt").html(checkedCnt + "개가 선택되었습니다.");
         $("body").focus();
-        */
+
         $(".qaData").each(function() {
             $(this).removeClass("btn-select");
             $(this).addClass("btn-white");
@@ -33,6 +132,8 @@ $(function() {
 
 
     });
+
+
     $(".nextStep").click(function() {
         var debugUrl = "";
 
@@ -43,24 +144,18 @@ $(function() {
         var brandCnt = $("#brandCnt").val();
         var qaData = [];
         for(var i = 0; i<brandCnt; i++){
-
-            if($(".qaData_"+i).is(":checked") == false) {
-                alert("응답하지 않은 설문이 있습니다.");
-                return;
-            } else {
-                $(".qaData_"+i).each(function() {
-                    $(this).each(function() {
-                        if($(this).is(":checked") == true) {
-                            var data = {
-                                "brandCode": $(this).attr("brand_code")
-                                , "qaCode": $(this).val()
+            $(".qaData_"+i).each(function() {
+                        $(this).each(function() {
+                            if($(this).is(":checked") == true) {
+                                var data = {
+                                    "brandCode": $(this).attr("brand_code")
+                                    , "qaCode": $(this).val()
+                                }
+                                qaData.push(data);
                             }
-                            qaData.push(data);
-                        }
-                    });
+                        });
 
-                });
-            }
+                    });
         }
 
 
@@ -75,9 +170,6 @@ $(function() {
             ,q_code : $("#q_code").val()
             ,qaData : JSON.stringify(qaData)
         };
-
-        console.log(json);
-        return;
 
 
 
@@ -95,4 +187,5 @@ $(function() {
         }
 
     });
+    */
 });
