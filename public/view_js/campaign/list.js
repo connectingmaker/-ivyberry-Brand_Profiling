31e27ebfac216b5a32da8867902f6ac6dbafdb1f
kwangheejung <khjung@c-maker.co.kr>
2018-01-25 10:38:46 +0900

    [설문관리] - 참여자수 표기문제 1. 현재 퀘스트 진행된 숫자로 표기 2. 퀘스트 별 지급포인트

--- public/view_js/campaign/list.js
+++ public/view_js/campaign/list.js
@@ -69,6 +69,52 @@ $(function() {
         $("#searchForm").submit();
     });
 
+    $(".data").click(function() {
+        var campaign_code = $(this).parents("tr").attr("id");
+        var json = {
+            campaign_code : campaign_code
+        };
+        common.ajax.send("/statistics/campaignData", json);
+        common.ajax.return = function(returnData) {
+            console.log(returnData);
+            $(".dataViewtd").css({"background-color":"#fff", "color": "#646464"});
+            $("#"+campaign_code).css({
+                "background-color":"#DA420B"
+                , "color":"#fff"
+            })
+            $(".dataView").hide();
+            $("#dataView_"+campaign_code).show(500);
+
+            var template = "";
+            var total = 0;
+            var total_point = 0;
+            for(var i = 0; i<returnData.length; i++) {
+                template += "<tr>";
+                template += "<td class='text-center'>"+returnData[i].QUEST_NUM+"차</td>";
+                template += "<td class='text-center'>"+returnData[i].TOTAL+"</td>";
+                template += "<td class='text-center'>"+returnData[i].SUM_POINT+"</td>";
+                template += "<td class='text-center'><a href='/campaign/campaignRawData/"+returnData[i].CAMPAIGN_CODE+"/"+returnData[i].QUEST_NUM+"' class=\"btn btn-xs btn-danger\">다운로드</a>&nbsp;<a href='/statistics/group/"+returnData[i].CAMPAIGN_CODE+"/"+returnData[i].QUEST_NUM+"' class=\"btn btn-xs btn-danger\">통계</a></td>";
+                template += "</tr>";
+
+                total = total + returnData[i].TOTAL;
+                total_point = total_point + returnData[i].SUM_POINT;
+            }
+            template += "<tr>";
+            template += "<td class='text-center bg-city-dark text-white'>합계</td>";
+            template += "<td class='text-center bg-city-dark text-white'>"+total+"</td>";
+            template += "<td class='text-center bg-city-dark text-white'>"+total_point+"</td>";
+            template += "<td class='text-center bg-city-dark text-white'></td>"
+            template += "</tr>";
+
+
+            $("#dataView_"+campaign_code).find("table").find("tbody").html(template);
+
+
+        }
+
+
+    });
+
 
 
 
--- public/view_js/statistics/group.js
+++ public/view_js/statistics/group.js
@@ -27,6 +27,8 @@ $(function() {
             $("#"+campaign_code).show(500);
 
             var template = "";
+            var total = 0;
+            var total_point = 0;
             for(var i = 0; i<returnData.length; i++) {
                 template += "<tr>";
                 template += "<td class='text-center'>"+returnData[i].QUEST_NUM+"차</td>";
@@ -34,7 +36,16 @@ $(function() {
                 template += "<td class='text-center'>"+returnData[i].SUM_POINT+"</td>";
                 template += "<td class='text-center'><a href='/campaign/campaignRawData/"+returnData[i].CAMPAIGN_CODE+"/"+returnData[i].QUEST_NUM+"' class=\"btn btn-xs btn-danger\">다운로드</a>&nbsp;<a href='/statistics/group/"+returnData[i].CAMPAIGN_CODE+"/"+returnData[i].QUEST_NUM+"' class=\"btn btn-xs btn-danger\">통계</a></td>";
                 template += "</tr>";
+
+                total = total + returnData[i].TOTAL;
+                total_point = total_point + returnData[i].SUM_POINT;
             }
+            template += "<tr>";
+            template += "<td class='text-center bg-city-dark text-white'>합계</td>";
+            template += "<td class='text-center bg-city-dark text-white'>"+total+"</td>";
+            template += "<td class='text-center bg-city-dark text-white'>"+total_point+"</td>";
+            template += "<td class='text-center bg-city-dark text-white'></td>"
+            template += "</tr>";
 
 
             $("#"+campaign_code).find("table").find("tbody").html(template);
